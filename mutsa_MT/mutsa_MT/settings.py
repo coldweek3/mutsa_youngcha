10db801b19c5cede2b0da392b5d8d25ef2714210
dudtlstm <annpys1024@naver.com>
2024-05-16 21:40:39 +0900

    🩹 fix: (영신 좋으라고..) static code 모아두기

--- mutsa_MT/mutsa_MT/settings.py
+++ mutsa_MT/mutsa_MT/settings.py
@@ -15,11 +15,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_URL = 'static/'
-STATICFILES_DIRS =  [
-        os.path.join(BASE_DIR, 'mutsa_MT', 'static' )
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,6 +118,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS =  [
+        os.path.join(BASE_DIR, 'mutsa_MT', 'static' )
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
